2beafa8b510ae93ea49252c0b89e2d77adf8b938
Eduardo Sena <eduardosena@hotmail.com.br>
2017-07-14 22:45:57 -0300

    Export-TaskSequence

---
+++ Export-TaskSequence.ps1
@@ -0,0 +1,21 @@
+﻿[CmdletBinding()]
+Param (
+    [Parameter (Mandatory=$True)]
+        $DSPath,
+    [Parameter (Mandatory=$True)]
+        $TSID,
+    [Parameter (Mandatory=$True)]
+        $NewTemplateName,
+    [Parameter (Mandatory=$true)]
+        $Description
+)
+
+Copy-Item -Path "$DSPath\Control\$TSID\ts.xml" -Destination "$DSPath\Templates\$($NewTemplateName).xml"
+   
+$fileName = "$DSPath\Templates\$($NewTemplateName).xml"
+
+$xmlfile = [System.Xml.XmlDocument](Get-Content $fileName)
+$XMLAtt = $xmlfile.sequence
+$XMLAtt.SetAttribute("name","$NewTemplateName")
+$XMLAtt.SetAttribute("description","$Description")
+$xmlfile.Save($fileName)
